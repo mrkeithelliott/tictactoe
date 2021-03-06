9e92cf777c93001911aefb92be6530399016d0dc
Ismael Perez <ismael.perezx@gmail.com>
2021-03-05 23:27:41 -0800

    Upgrade to Swift 5.0 also compatible with 4.0

--- TicTacToe/GameStateMachine.swift
+++ TicTacToe/GameStateMachine.swift
@@ -21,13 +21,13 @@ class StartGameState: GKState{
         super.init()
     }
     
-    override func isValidNextState(stateClass: AnyClass) -> Bool {
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
         return stateClass == ActiveGameState.self
     }
     
-    override func updateWithDeltaTime(seconds: NSTimeInterval) {
+    override func update(deltaTime seconds: TimeInterval) {
         resetGame()
-        self.stateMachine?.enterState(ActiveGameState.self)
+        self.stateMachine?.enter(ActiveGameState.self)
     }
     
     func resetGame(){
@@ -41,20 +41,20 @@ class StartGameState: GKState{
         let bottom_middle: BoardCell = BoardCell(value: .None, node: "//*bottom_middle")
         let bottom_right: BoardCell = BoardCell(value: .None, node: "//*bottom_right")
         
-        boardNode = self.scene?.childNodeWithName("//Grid") as? SKSpriteNode
+        boardNode = self.scene?.childNode(withName: "//Grid") as? SKSpriteNode
         
-        winningLabel = self.scene?.childNodeWithName("winningLabel")
-        winningLabel.hidden = true
+        winningLabel = self.scene?.childNode(withName: "winningLabel")
+        winningLabel.isHidden = true
         
-        resetNode = self.scene?.childNodeWithName("Reset")
-        resetNode.hidden = true
+        resetNode = self.scene?.childNode(withName: "Reset")
+        resetNode.isHidden = true
         
         
         let board = [top_left, top_middle, top_right, middle_left, center, middle_right, bottom_left, bottom_middle, bottom_right]
         
         self.scene?.gameBoard = Board(gameboard: board)
         
-        self.scene?.enumerateChildNodesWithName("//grid*") { (node, stop) in
+        self.scene?.enumerateChildNodes(withName: "//grid*") { (node, stop) in
             if let node = node as? SKSpriteNode{
                 node.removeAllChildren()
             }
@@ -70,17 +70,17 @@ class EndGameState: GKState{
         super.init()
     }
     
-    override func isValidNextState(stateClass: AnyClass) -> Bool {
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
         return stateClass == StartGameState.self
     }
     
-    override func didEnterWithPreviousState(previousState: GKState?) {
+    override func didEnter(from previousState: GKState?) {
         updateGameState()
     }
     
     func updateGameState(){
-        let resetNode = self.scene?.childNodeWithName("Reset")
-        resetNode?.hidden = false
+        let resetNode = self.scene?.childNode(withName: "Reset")
+        resetNode?.isHidden = false
     }
 }
 
@@ -94,15 +94,15 @@ class ActiveGameState: GKState{
         super.init()
     }
     
-    override func isValidNextState(stateClass: AnyClass) -> Bool {
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
         return stateClass == EndGameState.self
     }
     
-    override func didEnterWithPreviousState(previousState: GKState?) {
+    override func didEnter(from previousState: GKState?) {
         waitingOnPlayer = false
     }
     
-    override func updateWithDeltaTime(seconds: NSTimeInterval) {
+    override func update(deltaTime seconds: TimeInterval) {
         assert(scene != nil, "Scene must not be nil")
         assert(scene?.gameBoard != nil, "Gameboard must not be nil")
         
@@ -118,14 +118,14 @@ class ActiveGameState: GKState{
         
         let (state, winner) = self.scene!.gameBoard!.determineIfWinner()
         if state == .Winner{
-            let winningLabel = self.scene?.childNodeWithName("winningLabel")
-            winningLabel?.hidden = true
-            let winningPlayer = self.scene!.gameBoard!.isPlayerOne(winner!) ? "1" : "2"
+            let winningLabel = self.scene?.childNode(withName: "winningLabel")
+            winningLabel?.isHidden = true
+            let winningPlayer = self.scene!.gameBoard!.isPlayerOne(player: winner!) ? "1" : "2"
             if let winningLabel = winningLabel as? SKLabelNode,
-                let player1_score = self.scene?.childNodeWithName("//player1_score") as? SKLabelNode,
-                let player2_score = self.scene?.childNodeWithName("//player2_score") as? SKLabelNode{
+               let player1_score = self.scene?.childNode(withName: "//player1_score") as? SKLabelNode,
+               let player2_score = self.scene?.childNode(withName: "//player2_score") as? SKLabelNode{
                 winningLabel.text = "Player \(winningPlayer) wins!"
-                winningLabel.hidden = false
+                winningLabel.isHidden = false
                 
                 if winningPlayer == "1"{
                     player1_score.text = "\(Int(player1_score.text!)! + 1)"
@@ -134,31 +134,31 @@ class ActiveGameState: GKState{
                     player2_score.text = "\(Int(player2_score.text!)! + 1)"
                 }
                 
-                self.stateMachine?.enterState(EndGameState.self)
+                self.stateMachine?.enter(EndGameState.self)
                 waitingOnPlayer = false
             }
         }
         else if state == .Draw{
-            let winningLabel = self.scene?.childNodeWithName("winningLabel")
-            winningLabel?.hidden = true
+            let winningLabel = self.scene?.childNode(withName: "winningLabel")
+            winningLabel?.isHidden = true
             
             
             if let winningLabel = winningLabel as? SKLabelNode{
                 winningLabel.text = "It's a draw"
-                winningLabel.hidden = false
+                winningLabel.isHidden = false
             }
-            self.stateMachine?.enterState(EndGameState.self)
+            self.stateMachine?.enter(EndGameState.self)
             waitingOnPlayer = false
         }
 
         else if self.scene!.gameBoard!.isPlayerTwoTurn(){
             //AI moves
-            self.scene?.userInteractionEnabled = false
+            self.scene?.isUserInteractionEnabled = false
             
             assert(scene != nil, "Scene must not be nil")
             assert(scene?.gameBoard != nil, "Gameboard must not be nil")
                 
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            DispatchQueue.main.async{
                 self.scene!.ai.gameModel = self.scene!.gameBoard!
                 let move = self.scene!.ai.bestMoveForActivePlayer() as? Move
                     
@@ -166,27 +166,27 @@ class ActiveGameState: GKState{
                     
                 let strategistTime = CFAbsoluteTimeGetCurrent()
                 let delta = CFAbsoluteTimeGetCurrent() - strategistTime
-                let  aiTimeCeiling: NSTimeInterval = 1.0
+                let  aiTimeCeiling: TimeInterval = 1.0
                     
                 let delay = min(aiTimeCeiling - delta, aiTimeCeiling)
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay) * Int64(NSEC_PER_SEC)), dispatch_get_main_queue()) {
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delay ){
                         
-                    guard let cellNode: SKSpriteNode = self.scene?.childNodeWithName(self.scene!.gameBoard!.getElementAtBoardLocation(move!.cell).node) as? SKSpriteNode else{
+                    guard let cellNode: SKSpriteNode = self.scene?.childNode(withName: self.scene!.gameBoard!.getElementAtBoardLocation(index: move!.cell).node) as? SKSpriteNode else{
                             return
                     }
                     let circle = SKSpriteNode(imageNamed: "O_symbol")
                     circle.size = CGSize(width: 75, height: 75)
                     cellNode.addChild(circle)
-                    self.scene!.gameBoard!.addPlayerValueAtBoardLocation(move!.cell, value: .O)
+                    self.scene!.gameBoard!.addPlayerValueAtBoardLocation(index: move!.cell, value: .O)
                     self.scene!.gameBoard!.togglePlayer()
                     self.waitingOnPlayer = false
-                    self.scene?.userInteractionEnabled = true
+                    self.scene?.isUserInteractionEnabled = true
                 }
             }
         }
         else{
             self.waitingOnPlayer = false
-            self.scene?.userInteractionEnabled = true
+            self.scene?.isUserInteractionEnabled = true
         }
     }
 }
